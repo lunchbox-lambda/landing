046869450798fb880ccea168d4c083a6ef23013a
Zoltan Ujvary <ujvzolee@gmail.com>
2018-06-24 17:45:39 +0200

    Download section draft

--- src/components/download/index.tsx
+++ src/components/download/index.tsx
@@ -1,13 +1,35 @@
 import './style.scss'
-import app from 'lib/app'
 import * as React from 'react'
 import { Element } from 'react-scroll'
+import * as ReactMarkdown from 'react-markdown'
 import { Component } from 'components/common'
 
 interface Props { }
 
 interface State { }
 
+const installInstructions = `
+### 1) Install Docker on the Raspberry Pi
+\`\`\`
+curl -sSL get.docker.com | sh
+\`\`\`
+
+\`\`\`
+sudo usermod -aG docker pi
+\`\`\`
+
+\`\`\`
+sudo systemctl enable docker
+\`\`\`
+
+\`\`\`
+sudo systemctl start docker
+\`\`\`
+
+\`\`\`
+sudo pip install docker-compose
+\`\`\`
+`
 export class Download extends Component<Props, State> {
 
   constructor(props: Props) {
@@ -16,8 +38,7 @@ export class Download extends Component<Props, State> {
 
   componentDidMount() {
     this.handleSubscriptions([
-      // app.store.getState()
-      //   .subscribe(store => console.log(store))
+
     ])
   }
 
@@ -28,6 +49,18 @@ export class Download extends Component<Props, State> {
 
           <div className="section-header text-center">
             <h2 className="title">Download</h2>
+          </div>
+
+          <div className="description-wrapper">
+            <div className="install-instructions">
+              <ReactMarkdown source={ installInstructions } />
+            </div>
+            <div className="download-instructions">
+              <button className="main-btn">Download the brain</button>
+              <div className="coming-soon">
+                <div></div>
+              </div>
+            </div>
           </div>
 
         </div>
