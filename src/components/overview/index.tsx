24527d9519a2b002493665a9dcd85840be727bcf
Zoltan Ujvary <ujvzolee@gmail.com>
2018-06-25 18:46:05 +0200

    Redesigned the Overview screen

--- src/components/overview/index.tsx
+++ src/components/overview/index.tsx
@@ -17,16 +17,51 @@ export class Overview extends Component<Props, State> {
     return <Element name="overview" className="section md-padding">
       <div className="container">
         <div className="row">
-
-          <ul style={ { listStyleType: 'disc' } }>
-            <li>Grow your own food and harvest all around the year</li>
-            <li>Furnish your apartement with a unique modern design</li>
-            <li>Make a fun weekend DIY project with your kids</li>
-            <li>Grow your own food and harvest all around the year</li>
-            <li>Furnish your apartement with a unique modern design</li>
-            <li>Make a fun weekend DIY project with your kids</li>
-          </ul>
-
+          <div className="description-wrapper">
+            <div className="inspiration-list">
+              <div>
+                <i className="fa fa-lg fa-leaf"></i>
+                Grow your own food and harvest all around the year
+              </div>
+              <div>
+                <i className="fa fa-lg fa-leaf"></i>
+                Furnish your apartement with a unique modern design
+              </div>
+              <div>
+                <i className="fa fa-lg fa-leaf"></i>
+                Make a fun weekend DIY project with your kids
+              </div>
+              <div>
+                <i className="fa fa-lg fa-leaf"></i>
+                Grow your own food and harvest all around the year
+              </div>
+              <div>
+                <i className="fa fa-lg fa-leaf"></i>
+                Furnish your apartement with a unique modern design
+              </div>
+              <div>
+                <i className="fa fa-lg fa-leaf"></i>
+                Make a fun weekend DIY project with your kids
+              </div>
+            </div>
+            <div className="instruction-list">
+              { [0, 1, 2].map(n =>
+                <div key={ n } className="paragraph">
+                  <div className="bullet-part">
+                    <div className="bullet"></div>
+                    <div className="lines">
+                      <div className="line"></div>
+                      <div className="line"></div>
+                      <div className="line"></div>
+                    </div>
+                  </div>
+                  <div className="lines">
+                    <div className="line" style={ { marginLeft: '0px' } }></div>
+                  </div>
+                </div>
+              ) }
+            </div>
+          </div>
         </div>
       </div>
     </Element>
